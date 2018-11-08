9b875e3c70308e77d8f4e9184f06f3f41a8f06ee
thvuongtmu <m716106006@tmu.edu.tw>
2018-11-08 21:54:38 +0800

    plot function added

--- actogram/actogram.py
+++ actogram/actogram.py
@@ -2,17 +2,59 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pylab as plt
 import matplotlib
-import matplotlib.animation as animation
+matplotlib.use('Qt5Agg')
+import matplotlib.pylab as plt
+# import matplotlib.animation as animation
 from matplotlib import style
+from matplotlib.colors import ListedColormap
+from matplotlib.pyplot import figure
+from matplotlib.widgets import Slider
 style.use('seaborn-colorblind')
 
-box = 'BOX1'
-pir = 'PIR01'
-led = 'LED01'
 
-df = pd.read_table('BOX2-COM4-20181018.txt', sep='\s+',
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+# Add a scollable window solution (https://stackoverflow.com/questions/42622146/scrollbar-on-matplotlib-showing-page)
+
+
+class ScrollableWindow(QtWidgets.QMainWindow):
+    def __init__(self, fig):
+        self.qapp = QtWidgets.QApplication([])
+
+        QtWidgets.QMainWindow.__init__(self)
+        self.widget = QtWidgets.QWidget()
+        self.setCentralWidget(self.widget)
+        self.widget.setLayout(QtWidgets.QVBoxLayout())
+        self.widget.layout().setContentsMargins(0, 0, 0, 0)
+        self.widget.layout().setSpacing(0)
+
+        self.fig = fig
+        self.canvas = FigureCanvas(self.fig)
+        self.canvas.draw()
+        self.scroll = QtWidgets.QScrollArea(self.widget)
+        self.scroll.setWidget(self.canvas)
+
+        self.nav = NavigationToolbar(self.canvas, self.widget)
+        self.widget.layout().addWidget(self.nav)
+        self.widget.layout().addWidget(self.scroll)
+
+        self.show()
+        exit(self.qapp.exec_())
+
+
+pd.set_option("display.max_rows", None)
+
+box = 'BOX2'
+pir = 'PIR02'
+led = 'LED02'
+#filename = 'BOX1-3-20181018.txt'
+#filename = 'BOX2-COM4-20181018.txt'
+filename = 'BOX2-COM4-20181018.txt'
+
+df = pd.read_table(filename, sep='\s+',
                    skiprows=23, index_col=None)
 df.index = pd.to_datetime(df['MO/DY/YEAR']+' ' + df['HH:MM:SS'],
                           format="%m/%d/%Y %H:%M:%S")
@@ -50,9 +92,10 @@ df3.index = pd.to_datetime(df3['MO/DY/YEAR']+' ' + df3['HH:MM:SS'],
 df3.index.set_value(df3.index, df3.index[0], pd.Timestamp(
     df3.index.date[0].year, df3.index.date[0].month, df3.index.date[0].day, df3.index.time[0].hour, df3.index.time[0].minute+1, df3.index.time[0].second))
 
-
 df = pd.concat([df0, df1, df, df3, df2])
 dategroup = df.groupby(pd.Grouper(freq='D'))
+dategroup2 = df2.groupby(pd.Grouper(freq='D'))
+
 
 k = 0
 df2 = pd.DataFrame()
@@ -63,46 +106,67 @@ for name, group in dategroup:
     k = k+1
 
 # Remove the margins
-dategroup2 = df2.groupby(pd.Grouper(freq='D'))
+
 plt.rcParams['axes.autolimit_mode'] = 'round_numbers'
 plt.rcParams['axes.xmargin'] = 0.
-plt.rcParams['axes.ymargin'] = 0.
-# plt.rcParams['xtick.direction'] = 'out'
+plt.rcParams['axes.ymargin'] = 0.1
+#plt.rcParams['xtick.direction'] = 'out'
+plt.rcParams['axes.linewidth'] = 0.5  # axis thickness
+plt.rcParams['font.family'] = ['sans serif']
+plt.rcParams['font.size'] = 10
 
 n_group = dategroup.ngroups
 
 fig, axes = plt.subplots(nrows=n_group, ncols=2)
 
+# Half-opaque grayscale colormap
+# by Bart https://stackoverflow.com/questions/37327308/add-alpha-to-an-existing-matplotlib-colormap
+cmap = plt.cm.gray
+my_cmap = cmap(np.arange(cmap.N))
+my_cmap[:, -1] = np.linspace(0.2, 1, cmap.N)
+my_cmap = ListedColormap(my_cmap)
+
+# scale to 1000 if max PIR is 60
+scale = 1000/max(group[pir])
+
+# Double-plot actogram
 # Plot the 1st column
 j = 0
 for name, group in dategroup:
-    group[pir].plot.area(ax=axes[j, 0], sharey=True, fontsize=8)
-    (group[led]*500).plot.area(linewidth=0, ax=axes[j, 0],
-                               colormap="Pastel1", sharey=True)
+    (group[pir]*scale).plot.area(ax=axes[j, 0],
+                                 sharey=True, cmap='gray', figsize=(5, 0.3*n_group))
+    ((1-group[led])*800).plot.area(linewidth=0, ax=axes[j, 0],
+                                   cmap=my_cmap, sharey=True)
     axes[j, 0].axes.set_yticklabels([])
-    axes[j, 0].axes.set_xticklabels([0, 3, 6, 9, 12, 15, 18, 21])
-    axes[j, 0].axes.set_xlabel('Time')
+    axes[j, 0].axes.set_yticks([])
+    axes[j, 0].axes.set_xticklabels(
+        [0, 3, 6, 9, 12, 15, 18, 21, 24], rotation=0, size=7.5)
+    axes[j, 0].axes.set_ylim(1, 800)
+    axes[j, 0].axes.set_xlabel('Hour of day', rotation=0, size=9)
     axes[j, 0].axes.set_ylabel(
-        str(group[pir].index.date[0].day)+'/' + str(group[pir].index.date[0].month), rotation=0, size=8)
+        str(group[pir].index.date[0].month) + '/' + str(group[pir].index.date[0].day) + ' ', rotation=0, size=9)
+    axes[j, 0].yaxis.set_label_coords(-0.125, 0.0)
     if j < n_group-1:
         x_axis = axes[j, 0].axes.get_xaxis()
         x_axis.set_visible(False)
     j = j+1
-
 # Plot the 2nd column
 i = 0
 for name, group in dategroup2:
-    group[pir].plot.area(ax=axes[i, 1], sharey=True)
-    (group[led]*500).plot.area(linewidth=0,
-                               colormap="Pastel1", ax=axes[i, 1], sharey=True)
+    (group[pir]*scale).plot.area(ax=axes[i, 1],
+                                 sharey=True, cmap='gray', figsize=(5, 0.3*n_group))
+    ((1-group[led])*800).plot.area(linewidth=0,
+                                   cmap=my_cmap, ax=axes[i, 1], sharey=True)
     x_axis = axes[i, 1].axes.get_xaxis()
     x_axis.set_visible(False)
+    axes[i, 1].axes.set_ylim(1, 800)
     y_axis = axes[i, 1].axes.get_yaxis()
     y_axis.set_visible(False)
     i = i+1
 
-fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, wspace=0, hspace=0)
+fig.subplots_adjust(left=0.12, right=0.88, bottom=0.45, wspace=0, hspace=0)
 plt.axis('off')
-plt.suptitle(box)
+plt.suptitle(box, size=9)
 plt.savefig(box+'.png')
-plt.show()
+# plt.show()
+a = ScrollableWindow(fig)
