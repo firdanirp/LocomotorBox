5b54879e8d8c02525b46865d74331c92aebfa655
thvuongtmu <m716106006@tmu.edu.tw>
2018-11-08 22:17:35 +0800

    LocoBox-Acto fixed

--- LocoBox-Acto.py
+++ LocoBox-Acto.py
@@ -13,9 +13,31 @@ try:
     from tkinter import filedialog
 except ImportError:
     fileDialog = tk.filedialog
-import threading #to run Arduino loop and tkinter loop alongside
-import serial.tools.list_ports #for identifying Arduino port
+import threading # To run Arduino loop and tkinter loop alongside
+import serial.tools.list_ports # For identifying Arduino port
 import numpy as np
+import pandas as pd
+import matplotlib # For plotting actogram
+# matplotlib.use('Qt5Agg')
+import matplotlib.pylab as plt
+from matplotlib import style
+from matplotlib.colors import ListedColormap
+# from matplotlib.pyplot import figure
+# from matplotlib.widgets import Slider
+# style.use('seaborn-colorblind') # For color blinders
+# from PyQt5 import QtWidgets
+# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+# from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+# Remove the margins
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+plt.rcParams['axes.autolimit_mode'] = 'round_numbers'
+plt.rcParams['axes.xmargin'] = 0.
+plt.rcParams['axes.ymargin'] = 0.1
+#plt.rcParams['xtick.direction'] = 'out'
+plt.rcParams['axes.linewidth'] = 0.5  # axis thickness
+plt.rcParams['font.family'] = ['sans serif']
+plt.rcParams['font.size'] = 10
+
 
 # Global variables
 global hourOn1_1, minOn1_1, hourOff1_1, minOff1_1, hourOn2_1, minOn2_1, hourOff2_1, minOff2_1 
@@ -128,7 +150,7 @@ class StatusBar(Frame): # scan open serial ports
 window = Tk()
 window.title('LocoBox (10-box)')
 if sys.platform.startswith('win'):
-    window.geometry('730x420')
+    window.geometry('770x420')
 elif sys.platform.startswith('darwin'):
     window.geometry('1000x440')
 elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
@@ -577,6 +599,130 @@ def save_conf(): # Save schedule configuration
     writeToJSONFile(configfilename, config)
     status.pack(side='bottom', fill='x')
     status.set('Schedule configuration saved.')
+
+
+from tkinter.filedialog import askopenfilename
+def read_data(): # Read data from file for plotting
+    global file
+    status.pack(side='bottom', fill='x')
+    status.set('Reading the data...')
+    file = askopenfilename(filetypes=(("Text files", "*.txt"),
+                                      ("All files", "*.*")))
+
+
+def plot_box1():
+    box = 'BOX1'
+    pir = 'PIR01'
+    led = 'LED01'
+    df = pd.read_table(file, sep='\s+',
+                    skiprows=23, index_col=None, error_bad_lines=False)
+    df.index = pd.to_datetime(df['MO/DY/YEAR']+' ' + df['HH:MM:SS'],
+                            format="%m/%d/%Y %H:%M:%S")
+    df0 = pd.DataFrame(
+        {'HH:MM:SS': ['00:00:00'],  'MO/DY/YEAR':  [df['MO/DY/YEAR'][0]],
+        'LED01': [0], 'PIR01': [0], 'LED02': [0], 'PIR02': [0], 'LED03': [0], 'PIR03': [0], 'LED04': [0], 'PIR04': [0], 'LED05': [0], 'PIR05': [0], 'LED06': [0], 'PIR06': [0], 'LED07': [0], 'PIR07': [0], 'LED08': [0], 'PIR08': [0], 'LED09': [0], 'PIR09': [0], 'LED10': [0], 'PIR10': [0]
+        })
+    df0.index = pd.to_datetime(df0['MO/DY/YEAR']+' ' + df0['HH:MM:SS'],
+                            format="%m/%d/%Y %H:%M:%S")
+
+    df1 = pd.DataFrame(
+        {'HH:MM:SS': df['HH:MM:SS'][0],  'MO/DY/YEAR':  [df['MO/DY/YEAR'][0]],
+        'LED01': [0], 'PIR01': [0], 'LED02': [0], 'PIR02': [0], 'LED03': [0], 'PIR03': [0], 'LED04': [0], 'PIR04': [0], 'LED05': [0], 'PIR05': [0], 'LED06': [0], 'PIR06': [0], 'LED07': [0], 'PIR07': [0], 'LED08': [0], 'PIR08': [0], 'LED09': [0], 'PIR09': [0], 'LED10': [0], 'PIR10': [0]
+        })
+    df1.index = pd.to_datetime(df1['MO/DY/YEAR']+' ' + df1['HH:MM:SS'],
+                            format="%m/%d/%Y %H:%M:%S")
+    df1.index.set_value(df1.index, df1.index[0], pd.Timestamp(
+        df1.index.date[0].year, df1.index.date[0].month, df1.index.date[0].day, df1.index.time[0].hour, df1.index.time[0].minute-1, df1.index.time[0].second))
+
+    df2 = pd.DataFrame(
+        {'HH:MM:SS': ['00:00:00'],  'MO/DY/YEAR':  [df['MO/DY/YEAR'][-1]],
+        'LED01': [0], 'PIR01': [0], 'LED02': [0], 'PIR02': [0], 'LED03': [0], 'PIR03': [0], 'LED04': [0], 'PIR04': [0], 'LED05': [0], 'PIR05': [0], 'LED06': [0], 'PIR06': [0], 'LED07': [0], 'PIR07': [0], 'LED08': [0], 'PIR08': [0], 'LED09': [0], 'PIR09': [0], 'LED10': [0], 'PIR10': [0]
+        })
+    df2.index = pd.to_datetime(df2['MO/DY/YEAR']+' ' + df2['HH:MM:SS'],
+                            format="%m/%d/%Y %H:%M:%S")
+    df2.index.set_value(df2.index, df2.index[0], pd.Timestamp(
+        df2.index.date[0].year, df2.index.date[0].month, df2.index.date[0].day, 23, 59, 0))
+
+    df3 = pd.DataFrame(
+        {'HH:MM:SS': df['HH:MM:SS'][-1],  'MO/DY/YEAR':  [df['MO/DY/YEAR'][-1]],
+        'LED01': [0], 'PIR01': [0], 'LED02': [0], 'PIR02': [0], 'LED03': [0], 'PIR03': [0], 'LED04': [0], 'PIR04': [0], 'LED05': [0], 'PIR05': [0], 'LED06': [0], 'PIR06': [0], 'LED07': [0], 'PIR07': [0], 'LED08': [0], 'PIR08': [0], 'LED09': [0], 'PIR09': [0], 'LED10': [0], 'PIR10': [0]
+        })
+    df3.index = pd.to_datetime(df3['MO/DY/YEAR']+' ' + df3['HH:MM:SS'],
+                            format="%m/%d/%Y %H:%M:%S")
+    df3.index.set_value(df3.index, df3.index[0], pd.Timestamp(
+        df3.index.date[0].year, df3.index.date[0].month, df3.index.date[0].day, df3.index.time[0].hour, df3.index.time[0].minute+1, df3.index.time[0].second))
+
+    df = pd.concat([df0, df1, df, df3, df2])
+    dategroup = df.groupby(pd.Grouper(freq='D'))
+
+    k = 0
+    df2 = pd.DataFrame()
+    for name, group in dategroup:
+        if k >= 1:
+            a = group
+            df2 = pd.concat([df2, a], axis=0)
+        k = k+1
+    dategroup2 = df2.groupby(pd.Grouper(freq='D'))
+    n_group = dategroup.ngroups
+    fig, axes = plt.subplots(nrows=n_group, ncols=2)
+    # Half-opaque grayscale colormap
+    # by Bart https://stackoverflow.com/questions/37327308/add-alpha-to-an-existing-matplotlib-colormap
+    cmap = plt.cm.gray
+    my_cmap = cmap(np.arange(cmap.N))
+    my_cmap[:, -1] = np.linspace(0.2, 1, cmap.N)
+    my_cmap = ListedColormap(my_cmap)
+
+    # scale to 1000 if max PIR is 60
+    scale = 1000/max(group[pir])
+
+    # Double-plot actogram
+    # Plot the 1st column
+    j = 0
+    for name, group in dategroup:
+        (group[pir]*scale).plot.area(ax=axes[j, 0],
+                                    sharey=True, cmap='gray', figsize=(5, 0.3*n_group))
+        ((1-group[led])*800).plot.area(linewidth=0, ax=axes[j, 0],
+                                    cmap=my_cmap, sharey=True)
+        axes[j, 0].axes.set_yticklabels([])
+        axes[j, 0].axes.set_yticks([])
+        axes[j, 0].axes.set_xticklabels(
+            [0, 3, 6, 9, 12, 15, 18, 21, 24], rotation=0, size=7.5)
+        axes[j, 0].axes.set_ylim(1, 800)
+        axes[j, 0].axes.set_xlabel('Hour of day', rotation=0, size=9)
+        axes[j, 0].axes.set_ylabel(
+            str(group[pir].index.date[0].month) + '/' + str(group[pir].index.date[0].day) + ' ', rotation=0, size=9)
+        axes[j, 0].yaxis.set_label_coords(-0.125, 0.0)
+        if j < n_group-1:
+            x_axis = axes[j, 0].axes.get_xaxis()
+            x_axis.set_visible(False)
+        j = j+1
+    # Plot the 2nd column
+    i = 0
+    for name, group in dategroup2:
+        (group[pir]*scale).plot.area(ax=axes[i, 1],
+                                    sharey=True, cmap='gray', figsize=(5, 0.3*n_group))
+        ((1-group[led])*800).plot.area(linewidth=0,
+                                    cmap=my_cmap, ax=axes[i, 1], sharey=True)
+        x_axis = axes[i, 1].axes.get_xaxis()
+        x_axis.set_visible(False)
+        axes[i, 1].axes.set_ylim(1, 800)
+        y_axis = axes[i, 1].axes.get_yaxis()
+        y_axis.set_visible(False)
+        i = i+1
+
+    fig.subplots_adjust(left=0.12, right=0.88, bottom=0.45, wspace=0, hspace=0)
+    plt.axis('off')
+    plt.suptitle(box, size=9)
+    top = Toplevel()
+    top.wm_title("Actogram")
+    scrollbar = Scrollbar(top)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    canvas = FigureCanvasTkAgg(fig, master=top)
+    canvas.get_tk_widget().config(yscrollcommand=scrollbar.set)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side = TOP,  fill = BOTH,  expand = False)
+
+
 
 def read_conf(): # Read schedule configuration
     status.pack(side='bottom', fill='x')
@@ -2398,6 +2544,20 @@ if __name__ == '__main__':
     recordingmenu.add_separator()
     recordingmenu.add_command(label='Stop', command=disconnect)
     menu.add_cascade(label='Recording', menu=recordingmenu)
+    #create Plot menu
+    plotmenu = Menu(menu)
+    plotmenu.add_command(label='Load data', command=read_data)
+    plotmenu.add_command(label='Plot Box1', command=plot_box1)
+    # plotmenu.add_command(label='Plot Box2', command=plot_box2)
+    # plotmenu.add_command(label='Plot Box3', command=plot_box3)
+    # plotmenu.add_command(label='Plot Box4', command=plot_box4)
+    # plotmenu.add_command(label='Plot Box5', command=plot_box5)
+    # plotmenu.add_command(label='Plot Box6', command=plot_box6)
+    # plotmenu.add_command(label='Plot Box7', command=plot_box7)
+    # plotmenu.add_command(label='Plot Box8', command=plot_box8)
+    # plotmenu.add_command(label='Plot Box9', command=plot_box9)
+    # plotmenu.add_command(label='Plot Box10', command=plot_box10)
+    menu.add_cascade(label='Actogram Plot', menu=plotmenu)
     #create About menu
     aboutmenu = Menu(menu)
     aboutmenu.add_command(label='About LocoBox', command=about)
